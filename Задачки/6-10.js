1028a96f74e095067e6d25f090d81da8caf2f91c
Nodzegamon <morespa@mail.ru>
2019-03-06 11:32:50 +0300

    first commit

---
+++ Задачки/6-10.js
@@ -0,0 +1,11 @@
+/* №6. Посчитать площадь и периметр круга с заданным радиусом*/
+var r=5; 
+var s=Math.pow(r,2)*Math.PI;
+var p=2*Math.PI*r;
+console.log(s,p);
+
+/* №7. Найти максимальный элемент в массиве*/
+var arr= [1,2,3,85,-15];
+var m=Math.max.apply(null,arr);
+console.log(m);
+
